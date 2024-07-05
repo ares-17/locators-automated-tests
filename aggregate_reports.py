92d9f7ac56363d564cbe79a934d6a16748c5e0a6
Alessandro <79211449+ares-17@users.noreply.github.com>
2024-07-05 08:44:27 +0200

    Update aggregate_reports.py
    added windows support in aggregate_reports

--- aggregate_reports.py
+++ aggregate_reports.py
@@ -4,73 +4,83 @@ import shutil
 from configparser import ConfigParser
 import subprocess
 import glob
+import platform
+from pathlib import Path
 
 config = ConfigParser()
 config.read('../config.ini')
 
-zip_file_path = config.get('aggregate_reports', 'zip_location').strip('"')
-source_reports = config.get('aggregate_reports', 'source_reports').strip('"')
-final_report_destination_path = config.get('aggregate_reports', 'final_report_path').strip('"')
-old_final_report_file = config.get('aggregate_reports', 'old_final_report_file').strip('"')
+zip_file_path = Path(config.get('aggregate_reports', 'zip_location').strip('"'))
+source_reports = Path(config.get('aggregate_reports', 'source_reports').strip('"'))
+final_report_destination_path = Path(config.get('aggregate_reports', 'final_report_path').strip('"'))
+old_final_report_file = Path(config.get('aggregate_reports', 'old_final_report_file').strip('"'))
 
-zip_file_name = os.path.splitext(os.path.basename(zip_file_path))[0]
-extracted_folder = os.path.join(os.getcwd(), zip_file_name)
-target_folder = os.path.join(extracted_folder, 'target')
-jar_name = os.path.join(target_folder, 'unisciReportExcel-0.0.1-jarReportTest.jar')
-surefire_reports_path=os.path.join(target_folder, 'surefire-reports')
-final_report_path=os.path.join(target_folder, 'reportComplessivo.xls')
+zip_file_name = zip_file_path.stem
+extracted_folder = Path.cwd() / zip_file_name
+target_folder = extracted_folder / 'target'
+jar_name = target_folder / 'unisciReportExcel-0.0.1-jarReportTest.jar'
+surefire_reports_path = target_folder / 'surefire-reports'
+final_report_path = target_folder / 'reportComplessivo.xls'
+
+# Gestione percorsi troppo lunghi su Windows
+if platform.system() == "Windows":
+    extracted_folder = Path(f"\\\\?\\{extracted_folder}")
+    target_folder = Path(f"\\\\?\\{target_folder}")
+    surefire_reports_path = Path(f"\\\\?\\{surefire_reports_path}")
+    final_report_path = Path(f"\\\\?\\{final_report_path}")
+    final_report_destination_path = Path(f"\\\\?\\{final_report_destination_path}")
+    old_final_report_file = Path(f"\\\\?\\{old_final_report_file}")
 
 def remove_old_report_file():
-    old_report = os.path.join(extracted_folder, 'reportComplessivo')
-    if os.path.exists(old_report):
+    old_report = extracted_folder / 'reportComplessivo'
+    if old_report.exists():
         try:
-            os.remove(old_report)
-        except:
-            print("Error while deleting file: ", old_report)
+            old_report.unlink()
+        except Exception as e:
+            print(f"Error while deleting file {old_report}: {e}")
 
 def extract():
     try:
-        old_exracted_folder=os.path.join(os.getcwd(), 'unisciReportExcel')
-        if os.path.exists(old_exracted_folder):
-            shutil.rmtree(old_exracted_folder)
+        old_extracted_folder = Path.cwd() / 'unisciReportExcel'
+        if old_extracted_folder.exists():
+            shutil.rmtree(old_extracted_folder)
     except Exception as e:
-        print(f"Si è verificato un errore durante l'eliminazione della cartella: {e}")
+        print(f"Error while removing the folder: {e}")
 
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall(os.getcwd())
+        zip_ref.extractall(Path.cwd())
 
 def open_target():
-    os.makedirs(target_folder, exist_ok=True)
+    target_folder.mkdir(parents=True, exist_ok=True)
     os.chdir(target_folder)
 
 def remove_old_surefire_report_files():
     try:
-        if not os.path.exists(surefire_reports_path):
-            print(f"La cartella '{surefire_reports_path}' non esiste.")
+        if not surefire_reports_path.exists():
+            print(f"The folder '{surefire_reports_path}' does not exist.")
             return
 
-        for filename in os.listdir(surefire_reports_path):
-            file_path = os.path.join(surefire_reports_path, filename)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
+        for file_path in surefire_reports_path.iterdir():
+            if file_path.is_file():
+                file_path.unlink()
     except Exception as e:
-        print(f"Si è verificato un errore durante la rimozione dei file: {e}")
+        print(f"Error while removing files: {e}")
 
 def copy_reports():
-    os.makedirs(target_folder, exist_ok=True)
-    fileList = glob.glob(f'{source_reports}/*.xls')
-    for filePath in fileList:
-        shutil.copy(filePath, surefire_reports_path)
+    target_folder.mkdir(parents=True, exist_ok=True)
+    file_list = glob.glob(f'{source_reports}/*.xls')
+    for file_path in file_list:
+        shutil.copy(file_path, surefire_reports_path)
 
 def execute_jar():
-    subprocess.run(['java', '-jar', jar_name, surefire_reports_path, 'reportComplessivo'], check=True, capture_output=True)
+    subprocess.run(['java', '-jar', str(jar_name), str(surefire_reports_path), 'reportComplessivo'], check=True, capture_output=True)
 
 def copy_final_report():
     shutil.copy(final_report_path, final_report_destination_path)
 
 def remove_old_final_report():
-    if os.path.isfile(old_final_report_file):
-        os.remove(old_final_report_file)
+    if old_final_report_file.is_file():
+        old_final_report_file.unlink()
 
 extract()
 open_target()
